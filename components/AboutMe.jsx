aed2bfa4888cc4628e737947647e5135f2df4934
pek yen <pekyen100@hotmail.com>
2023-12-09 11:09:36 +0800

    Complete portfolio

---
+++ components/AboutMe.jsx
@@ -0,0 +1,105 @@
+import React from "react";
+import { useTransition, useState } from "react";
+import TabButton from "./TabButton";
+
+//1d
+const TAB_DATA = [
+    {
+        title: "Skills",
+        id: "skills",
+        content: (
+          <ul className="list-disc pl-2">
+                <li>Python</li>
+                <li>MySql</li>
+                <li>Microsoft Power BI</li>
+                <li>Tableau</li>
+                <li>Amazon Quicksight</li>
+                <li>SAS Enterprise Miner</li>
+                <li>R Programming Language</li>
+                <li>Java</li>
+                <li>JavaScript</li>
+                <li>HTML</li>
+                <li>CSS</li>
+                <li>PHP</li>
+          </ul>
+        ),
+    },
+    {
+        title: "Experiences",
+        id: "experiences",
+        content: (
+          <ul className="list-disc pl-2">
+            <li>Data Analyst Intern in Avinity Analytics</li>
+            <li>Google Data Analytics</li>
+            <li>Microsoft Certified Azure Fundamental</li>
+            <li>Microsoft Office Specialist</li>
+          </ul>
+        ),
+    },
+    {
+        title: "Education",
+        id: "education",
+        content: (
+          <ul className="list-disc pl-2">
+                <li>Asia Pacific University</li>
+                <li>SMJK Ave Maria Convent</li>
+          </ul>
+        ),
+    }
+];
+//1
+const AboutMe = () => {
+    //1b
+    const[tab, setTab] = useState("skills");
+    const[isPending, startTransition] = useTransition();
+    const handleTabChange = (id) => {
+        startTransition(() => {
+            setTab(id);
+        });
+    };
+    //1a
+    return(
+        <section className="text-white grid" id="aboutMe">
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-8 md:px-32 py-16 xl-gap-16 items-center">
+                <div>
+                    <img src="/AboutMe.png" alt="About Me" width={450} height={450} style={{ borderRadius: "25px" }}/>
+                </div>
+                <div className="mt-4 md:mt-0 text-left flex flex-col h-full">
+                    <h1 className="text-4xl font-bold text-white mb-4">About Me</h1>
+                    <p className="text-base text-justify lg:text-lg">An undergraduate student at Asia Pacific University taking a bachelor's in computer science with a specialism in data analytics, where I have been honing my skills in various technologies. Teachable fast-learning student as well as keen listener with dedication and enthusiasm in exploring new knowledge once stepping into the real world. </p>
+                    <div className="flex flex-row mt-8">
+{/* 1e */}
+                        <TabButton 
+                        selectTab={() => handleTabChange("skills")}
+                        active= {tab === "skills"}
+                        >
+                            
+                            Skills
+                        </TabButton>
+
+                        <TabButton 
+                        selectTab={() => handleTabChange("experiences")}
+                        active= {tab === "experiences"}
+                        >
+                            {" "}
+                            Experiences{" "}
+                        </TabButton>
+
+                        <TabButton 
+                        selectTab={() => handleTabChange("education")}
+                        active= {tab === "education"}
+                        >
+                            {" "}
+                            Education{" "}
+                        </TabButton>
+                        
+                    </div>
+                    <div className="mt-8">{TAB_DATA.find((t) => t.id === tab).content}</div>
+                </div>
+
+            </div>
+        </section>
+
+    );
+};
+export default AboutMe;
--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,28 @@
 import react from "react";
-import HeroSection from "@/components/HeroSection";
+import dynamic from "next/dynamic";
+import AchievementsSection from "@/components/AchievementsSection";
+import AboutMe from "@/components/AboutMe";
 import Project from "@/components/Project";
+import EmailSection from "@/components/EmailSection";
+import Footer from "@/components/Footer";
 
+//Server: not found error handler
+const HeroSection = dynamic(
+  () => import('../components/HeroSection'),
+  { ssr: false }
+);
 
 export default function Home(){
   return(
     <div className="flex min-h-screen flex-col">
-      <HeroSection></HeroSection>
-      <Project></Project>
+      <main>
+      <HeroSection/>
+      <AchievementsSection/>
+      <AboutMe/>
+      <Project/>
+      <EmailSection/>
+      </main>
+      <Footer/>
     </div>
   );
 }
