93639c52d910540259d6cc0d1601d36285b75912
pek yen <pekyen100@hotmail.com>
2023-12-09 10:11:11 +0800

    Update

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,13 @@
 import react from "react";
 import HeroSection from "@/components/HeroSection";
+import Project from "@/components/Project";
+
 
 export default function Home(){
   return(
     <div className="flex min-h-screen flex-col">
       <HeroSection></HeroSection>
+      <Project></Project>
     </div>
   );
 }
